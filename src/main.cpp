3b7703dc04c702e8f7770cea707df776436b6c9f
Juaneitor3000 <159396788+Juaneitor3000@users.noreply.github.com>
2025-03-15 21:02:48 -0500

    PID added

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,29 @@
 #include <max6675.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+#include <PID_v1.h>
 
 
-
+#define SSR_PIN 2 
+#define PERIOD_MS 1000
 int thermoDO = 19;
 int thermoCS = 23;
 int thermoCLK = 5;
 int sampleTime= 1000000;
+
+
+
+// Tiempo de referencia para el pulso de salida
+unsigned long previousMillis = 0;
+unsigned long pulseDuration = 0;
+
+
+
+// Variables PID
+double tempSetpointCelcius = 100.0;   // Temperatura deseada (°C)
+double tempReadCelcius = 0, output = 0;
+double Kp = 2.0, Ki = 5.0, Kd = 1.0;
+PID myPID(&tempReadCelcius, &output, &tempSetpointCelcius, Kp, Ki, Kd,DIRECT);
 
 
 
@@ -22,7 +38,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0,U8X8_PIN_NONE,/* SCL=*/22,/* SD
 
 
 // put function declarations here:
-void printScreen(float ,float);                       // PRINT SCREEN
+void printScreen(double ,double);                       // PRINT SCREEN
 
 
 
@@ -36,8 +52,10 @@ void IRAM_ATTR onTimer() {
 void setup() {
 // put your setup code here, to run once:
   
-Serial.begin(9600);  // Serial port init
+Serial.begin(115200);  // Serial port init
 u8g2.begin();        //Screen Init
+pinMode(SSR_PIN, OUTPUT); //output pin
+myPID.SetMode(AUTOMATIC);
 
 
 Serial.println("Temperature controller");
@@ -60,19 +78,40 @@ void loop() {
 
 if (pidFlag) {
     pidFlag = false;  // Reset flag
-    float tempSetpointCelcius=60;
-    float tempReadFarenheit = thermocouple.readFahrenheit();
-    float tempReadCelcius =  thermocouple.readCelsius();
-    Serial.print("C = "+ String(tempReadCelcius) +" F= "+ String(tempReadFarenheit));
+
+
+     // Leer temperatura y calcular PID
+    tempReadCelcius =  thermocouple.readCelsius();
+    unsigned long currentMillis = millis();
+    myPID.Compute();
+
+    // Calcular duración del pulso (entre 0 y PERIOD_MS)
+    pulseDuration = (output / 255.0) * PERIOD_MS;  
+
+    // Generar el pulso con millis()
+    if (currentMillis - previousMillis >= PERIOD_MS) {
+        previousMillis = currentMillis;  // Reiniciar ciclo
+        digitalWrite(SSR_PIN, HIGH);  // Encender SSR
+    }
+    
+    if (currentMillis - previousMillis >= pulseDuration) {
+        digitalWrite(SSR_PIN, LOW);  // Apagar SSR
+    }
+
+
+
+    Serial.print("C = "+ String(tempReadCelcius));
     printScreen(tempReadCelcius,tempSetpointCelcius);     //   PRINT SCREEN  
 }
   // put your main code here, to run repeatedly:
+
+
 
 }
 
 // put function definitions here:
 
-void printScreen(float tempRead,float tempSetpoint){
+void printScreen(double tempRead,double tempSetpoint){
 
   static const unsigned char image_download_bits[] = {0x38,0x00,0x44,0x40,0xd4,0xa0,0x54,0x40,0xd4,0x1c,0x54,0x06,0xd4,0x02,0x54,0x02,0x54,0x06,0x92,0x1c,0x39,0x01,0x75,0x01,0x7d,0x01,0x39,0x01,0x82,0x00,0x7c,0x00};
 
